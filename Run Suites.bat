e450f53ce9255e51e62c2ae5a1705f9395aea87f
Ben Luker <thebenluker@gmail.com>
2021-09-13 14:53:18 -0700

    Delete Run Suites.bat

--- Run Suites.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-if not exist "Virtual Environment"/ (echo Virtual environment does not exist. & call "Create Environment.bat")
-call "Virtual Environment"/Scripts/activate.bat
-robot --listener Resources/listener.py -d Results Tests
